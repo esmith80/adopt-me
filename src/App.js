d5748bda36f7705421f3dae22f10c58952126874
Edward Smith <edward.m.smith80@gmail.com>
2021-01-29 13:51:19 -0700

    set up tooling, base html and custom hook

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from 'react-dom';
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  // return React.createElement("div", { id: "something-important" },
+  //   [
+  //     React.createElement("h1", {}, "Adopt Me!"),
+  //     React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
+  //     React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
+  //     React.createElement(Pet, { name: "Kevin", animal: "Cat", breed: "Tuxedo" })
+  //   ]
+  // );
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
+};
+
+render(<App />, document.getElementById("root"));
