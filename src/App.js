53c86e1bc455c1de70f00af9c073d532d53af4d7
Edward Smith <edward.m.smith80@gmail.com>
2021-02-08 08:56:01 -0700

    add reach router and details

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Link } from '@reach/router';
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+
 
 const App = () => {
-  // return React.createElement("div", { id: "something-important" },
-  //   [
-  //     React.createElement("h1", {}, "Adopt Me!"),
-  //     React.createElement(Pet, { name: "Luna", animal: "Dog", breed: "Havanese" }),
-  //     React.createElement(Pet, { name: "Pepper", animal: "Bird", breed: "Cockatiel" }),
-  //     React.createElement(Pet, { name: "Kevin", animal: "Cat", breed: "Tuxedo" })
-  //   ]
-  // );
 
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+
+      <div>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    </React.StrictMode>
   );
 };
 
